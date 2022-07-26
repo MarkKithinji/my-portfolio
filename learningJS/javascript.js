495ff84915b46412235717b144603b7951e97361
MarkKithinji <105651925+MarkKithinji@users.noreply.github.com>
2022-07-26 16:22:48 +0300

    finished working on dictionary of places UI and logic, no work is done on database. started working on EATZ website

---
+++ learningJS/javascript.js
@@ -0,0 +1,10 @@
+let firstVal=0
+
+function numberIncrement() {
+    firstVal = firstVal+1
+} 
+
+function add() {
+    document.getElementById("countEl").innerText = firstVal;
+    document.getElementById("texts").innerHTML ="hello"
+}
--- public/DOPfiles/DOP.js
+++ public/DOPfiles/DOP.js
@@ -1,5 +1,5 @@
 
-let moveConstUp = document.getElementById("exploreBtnID");
+/*let moveConstUp = document.getElementById("exploreBtnID");
 
 
 let explore = document.getElementById("exploreContainer");
@@ -21,4 +21,44 @@ function move() {
     margin +=10;
 }
 
+*/
 
+
+
+//adding ripple to search button
+const btn = document.querySelector(".searchButton");
+  
+    // Listen for click event
+    btn.onclick = function (e) {
+  
+        // Create span element
+        let ripple = document.createElement("span");
+  
+        // Add ripple class to span
+        ripple.classList.add("ripple");
+  
+        // Add span to the button 
+        this.appendChild(ripple);
+  
+        // Get position of X
+        let x = e.clientX - e.target.offsetLeft;
+  
+        // Get position of Y 
+        let y = e.clientY - e.target.offsetTop;
+  
+        // Position the span element
+        ripple.style.left = `${x}px`;
+        ripple.style.top = `${y}px`;
+  
+        // Remove span after 0.3s
+        setTimeout(() => {
+            ripple.remove();
+        }, 6500);
+  
+    };
+
+
+//adding some function to the search button
+function search() {
+    document.getElementById("client_Response").innerText = "our engines are fetching the data. be patient"
+}
